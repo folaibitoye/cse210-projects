004f8f57f2fab15dbacef0241be5e3810709f194
folaibitoye <geocomconsultancy@gamil.com>
2024-05-31 16:43:18 +0100

    Final Journal Program

--- prove/Develop02/Program.cs
+++ prove/Develop02/Program.cs
@@ -8,8 +8,6 @@ namespace DailyJournal
     {
         static List<string> prompts = new List<string>
         {
-            // The last two prompts represents the creativity added
-            
             "Who was the most interesting person I interacted with today?",
             "What was the best part of my day?",
             "How did I see the hand of the Lord in my life today?",
